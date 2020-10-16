2b00064f9bbebf3b08a6e1db6c20aeabe17d4c71
skempas <sofia.kempas@hotmail.com>
2020-10-16 21:19:36 +0200

    add countdown timer

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { interval } from 'rxjs';
+
 
 @Component({
   selector: 'app-first',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstComponent implements OnInit {
 
+  _trialEndsAt;
+  _diff: number;
+   _days: number;
+   _hours: number;
+   _minutes: number;
+   _seconds: number;
+
   constructor() { }
 
-  ngOnInit(): void {
+  updateCountdown(){
+    this._diff = this._trialEndsAt - new Date().getTime();
+    this._days = this.getDays(this._diff);
+    this._hours = this.getHours(this._diff);
+    this._minutes = this.getMinutes(this._diff);
+    this._seconds = this.getSeconds(this._diff);
   }
+
+  ngOnInit(): void {
+    this._trialEndsAt = 1628337600000;
+    this.updateCountdown();
+    interval(1000).subscribe(res => {
+      this.updateCountdown();
+    })
+  }
+
+  getDays(t){
+    return Math.floor( t/(1000*60*60*24) );
+}
+
+getHours(t){
+    return Math.floor( (t/(1000*60*60)) % 24 );
+}
+
+getMinutes(t){
+    return Math.floor( (t/1000/60) % 60 );
+}
+
+getSeconds(t){
+    return Math.floor( (t/1000) % 60 );
+}
+
 
 }
